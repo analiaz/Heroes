22fa17df80abd30baf9ff6ffd7206a54205f79bb
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-05-09 22:36:36 -0300

    navBar init

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -1,17 +1,24 @@
 import { Route, Routes, Navigate} from 'react-router-dom'
 
-import { LoginPage } from '../auth/pages/LoginPage'
-import { DcPage } from '../heroes/pages/DcPage'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { Navbar } from '../ui'
+
+import { LoginPage } from '../auth'
+import { DcPage, MarvelPage } from '../heroes'
+
+
 
 export const AppRouter = () => {
   return (
-    <Routes>
-        <Route path='marvel' element={<MarvelPage />}/>
-        <Route path='dc' element={<DcPage />} />
-        <Route path='login' element={<LoginPage />} />
-        <Route path='/' element={<Navigate to="./marvel" />} />
-    </Routes>
+    <>
+      <Navbar />
+    
+      <Routes>
+          <Route path='marvel' element={<MarvelPage />}/>
+          <Route path='dc' element={<DcPage />} />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='/' element={<Navigate to="./marvel" />} />
+      </Routes>
+    </>
   )
 }
 
