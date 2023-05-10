ae773c5d73f7c06eb77a6ec3e52e8f7cfc217eae
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-05-10 16:07:29 -0300

    herocard

--- heroes-spa/src/heroes/components/HeroCard.jsx
+++ heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 export const HeroCard = ( { 
     id,
     superhero,
@@ -17,6 +17,24 @@ export const HeroCard = ( {
             <div className="row no-gutters">
                 <div className="col-4">
                     <img src={heroImagenUrl} alt={ superhero } className="card-img" />
+                </div>
+
+                <div className="col-8">
+                    <div className="card-body">
+                        <h5 className="card-title"> { superhero } </h5>
+                        <p className="card-text">{alter_ego}</p>
+
+                        {
+                            (alter_ego !== characters) && (<p> {characters} </p>) 
+                        }
+
+                        <p className="card-text">
+                            <small className="text-muted"> { first_appearance }</small>
+                        </p>
+
+                        <Link to={`/hero/${id}`}> Mas...</Link>
+                        
+                    </div>
                 </div>
             </div>
         </div>
